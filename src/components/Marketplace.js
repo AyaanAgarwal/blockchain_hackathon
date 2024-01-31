85d21a0f2d08c3f02e1ed439dfbc1edd7e457277
Ayaan Agarwal <ayaan08vk18@gmail.com>
2024-01-31 10:21:48 +0530

    final commit

--- src/components/Marketplace.js
+++ src/components/Marketplace.js
@@ -7,33 +7,33 @@ import { GetIpfsUrlFromPinata } from "../utils";
 
 export default function Marketplace() {
 const sampleData = [
-    {
-        "name": "NFT#1",
-        "description": "Alchemy's First NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#2",
-        "description": "Alchemy's Second NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
-    {
-        "name": "NFT#3",
-        "description": "Alchemy's Third NFT",
-        "website":"http://axieinfinity.io",
-        "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
-        "price":"0.03ETH",
-        "currentlySelling":"True",
-        "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
-    },
+    // {
+    //     "name": "NFT#1",
+    //     "description": "First NFT",
+    //     "website":"http://axieinfinity.io",
+    //     "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
+    //     "price":"0.03ETH",
+    //     "currentlySelling":"True",
+    //     "address":"0xe81Bf5A757CB4f7F82a2F23b1e59bE45c33c5b13",
+    // },
+    // {
+    //     "name": "NFT#2",
+    //     "description": "Second NFT",
+    //     "website":"http://axieinfinity.io",
+    //     "image":"https://gateway.pinata.cloud/ipfs/QmdhoL9K8my2vi3fej97foiqGmJ389SMs55oC5EdkrxF2M",
+    //     "price":"0.03ETH",
+    //     "currentlySelling":"True",
+    //     "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    // },
+    // {
+    //     "name": "NFT#3",
+    //     "description": "Third NFT",
+    //     "website":"http://axieinfinity.io",
+    //     "image":"https://gateway.pinata.cloud/ipfs/QmTsRJX7r5gyubjkdmzFrKQhHv74p5wT9LdeF1m3RTqrE5",
+    //     "price":"0.03ETH",
+    //     "currentlySelling":"True",
+    //     "address":"0xe81Bf5A757C4f7F82a2F23b1e59bE45c33c5b13",
+    // },
 ];
 const [data, updateData] = useState(sampleData);
 const [dataFetched, updateFetched] = useState(false);
@@ -47,6 +47,7 @@ async function getAllNFTs() {
     let contract = new ethers.Contract(MarketplaceJSON.address, MarketplaceJSON.abi, signer)
     //create an NFT Token
     let transaction = await contract.getAllNFTs()
+
 
     //Fetch all the details of every NFT from the contract and display
     const items = await Promise.all(transaction.map(async i => {
@@ -66,9 +67,11 @@ async function getAllNFTs() {
             name: meta.name,
             description: meta.description,
         }
+        console.log(item);
         return item;
     }))
 
+    console.log(items);
     updateFetched(true);
     updateData(items);
 }
@@ -92,4 +95,5 @@ return (
     </div>
 );
 
-}
+} 
+
